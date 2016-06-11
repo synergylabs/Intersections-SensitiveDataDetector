644837630d4444f1243302c116bf1759c0804791
z502185331 <lieyongzou@Lieyongs-MBP.wv.cc.cmu.edu>
2016-06-11 11:26:20 -0400

    Update
    (1) Use singleton to design a setting class, so all the components can
    load the same settings at the same time.

---
+++ src/capstone/sdd/core/Settings.java
@@ -0,0 +1,59 @@
+package capstone.sdd.core;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A class store all the settings of scanner
+ * @author lieyongzou
+ */
+public class Settings {
+	
+	private File start_folder;
+	private boolean scan_sub_repo;
+	private List<String> supported_file;
+	
+	private static Settings instance = null;
+	
+	public static Settings getInstance() {
+		if (instance == null) {
+			instance = new Settings();
+		}
+		
+		return instance;
+	}
+	
+	/**
+	 * A constructor set all the default settings
+	 */
+	private Settings() {
+		start_folder = new File("/");
+		scan_sub_repo = true;
+		supported_file = new ArrayList<>();
+	}
+
+	public File getStart_folder() {
+		return start_folder;
+	}
+
+	public void setStart_folder(File start_folder) {
+		this.start_folder = start_folder;
+	}
+
+	public boolean isScan_sub_repo() {
+		return scan_sub_repo;
+	}
+
+	public void setScan_sub_repo(boolean scan_sub_repo) {
+		this.scan_sub_repo = scan_sub_repo;
+	}
+
+	public List<String> getSupported_file() {
+		return supported_file;
+	}
+
+	public void addSupported_file(String suffix) {
+		this.supported_file.add(suffix);
+	}
+}
